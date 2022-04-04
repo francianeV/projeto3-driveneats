0755a8b6c3217075a430d6688124b6319d13f3fe
Franciane <franciane.penina@gmail.com>
2022-04-04 02:42:37 -0300

    Versão final do projeto

--- scripts.js
+++ scripts.js
@@ -152,12 +152,12 @@ function sendOrder(){
     const nome = prompt("Qual o seu nome? ");
     const endereco = prompt("Forneça o endereço de entrega: ")
 
-    const uri = `Olá, gostaria de fazer o pedido:\n
-    - Prato: ${plate}\n
-    - Bebida: ${drink}\n
-    - Sobremesa: ${dessert}\n 
-    Total: R$: ${totCompra.toFixed(2)}\n
-    Nome: ${nome}\n
+    const uri = `Olá, gostaria de fazer o pedido:
+    - Prato: ${plate}
+    - Bebida: ${drink}
+    - Sobremesa: ${dessert}
+    Total: R$: ${totCompra.toFixed(2)}
+    Nome: ${nome}
     Endereço: ${endereco}`
 
     window.open("https://wa.me/5512996768941?text=" + encodeURIComponent(uri));
